fa3307412b677273744b232a57b2b702af0939d8
shyam <shyam07052002@gmail.com>
2021-12-05 22:19:13 +0530

    added calender

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Calender } from './components/calender/Calender';
+
+const App=()=>{
+  return (
+    <div className="App">
+      <Calender/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/calender/Calender.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
+export const Calender = () => {
+  const [selectedDay, setSelectedDay] = useState(null);
+  return (
+    <Calendar
+      value={selectedDay}
+      onChange={setSelectedDay}
+      shouldHighlightWeekends
+    />
+  );
+}
