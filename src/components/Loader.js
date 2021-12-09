dc6c8b6dbfd293371dd28f46cf2d4305120d3c80
shyam <shyam07052002@gmail.com>
2021-12-09 16:43:48 +0530

    fixed calender component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,10 @@ export default class App extends React.Component {
   //other logic
   render() {
     return (
-        <Loader type="TailSpin" color="#00BFFF" height={80} width={80} />
+        <div className="loader">
+            <Loader type="TailSpin" color="#00BFFF" height={150} width={120} />
+        </div>  
     );
   }
 }
+
--- src/components/dashboard/Calender.js
+++ src/components/dashboard/Calender.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import "react-modern-calendar-datepicker/lib/DatePicker.css";
-import { Calendar } from "react-modern-calendar-datepicker";
+import '@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css';
+import {Calendar} from '@hassanmojab/react-modern-calendar-datepicker';
 import { useState,useEffect } from 'react';
 export const Calender = () => {
     const [selectedDay, setSelectedDay] = useState(null);
